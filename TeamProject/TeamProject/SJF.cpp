48c6f6cb665862ec43fbe003f456884446585fcb
Alex Nieto <alex.nieto7301@gmail.com>
2023-12-12 01:17:18 -0500

    Updated algo for pre-emptive solution (SRTN)

--- TeamProject/TeamProject/SJF.cpp
+++ TeamProject/TeamProject/SJF.cpp
@@ -24,6 +24,8 @@ void SJF :: implementAlg(){
     //set clock start time
     int clockTime = 0;
     
+    bool swapProcess = false;
+    
     //ready and wait queues
     list<Process> readyQueue;
     list<Event> waitingQueue;
@@ -44,10 +46,12 @@ void SJF :: implementAlg(){
     
     do{
         //loop through ready queue
-        for(Process process : readyQueue){
-            //getting all events arrived at that time
-            if(clockTime >= process.getArrivalTime()){
-                allProcessesArrived.push_back(process);
+        if(!swapProcess){
+            for(Process process : readyQueue){
+                //getting all events arrived at that time
+                if(clockTime >= process.getArrivalTime()){
+                    allProcessesArrived.push_back(process);
+                }
             }
         }
         //if no processes available at next time
@@ -58,31 +62,73 @@ void SJF :: implementAlg(){
         }else{
             //sort processes by service/burst time
             allProcessesArrived.sort([](Process &a, Process &b) { return a.getServiceTime() < b.getServiceTime(); });
+            if(swapProcess){
+                int oldClock = clockTime;
+                clockTime = allProcessesArrived.front().getArrivalTime();
+                nextReadyProcess.setServiceTime(nextReadyProcess.getServiceTime() - (clockTime - oldClock));
+                //create "swap" event -> return process to ready
+                currentEvent.setEvent(clockTime, nextReadyProcess, "ready", "running");
+                //adding event to events (all events that occur in algorithm)
+                events.push_back(currentEvent);
+                //return process to ready queue
+                readyQueue.push_back(nextReadyProcess);
+                clockTime+=switchTime;
+            }
             //sets next process to execute
             nextReadyProcess = allProcessesArrived.front();
             //set next process start time
             nextReadyProcess.setStartTime(clockTime);
             for(int i = 0; i < numOfProcesses; i++){
-                if(nextReadyProcess.getProcessId() == processes[i].getProcessId()){
+                if(nextReadyProcess.getProcessId() == processes[i].getProcessId() && !processes[i].getStarted()){
                     //set process start time for processes stored in process array
                     processes[i].setStartTime(clockTime);
+                    processes[i].setStarted(true);
                 }
             }
-            //clear list for next round
+            //create running event from next ready process
+            currentEvent.setEvent(clockTime, nextReadyProcess, "running", "ready");
+            //adding event to events (all events that occur in algorithm)
+            events.push_back(currentEvent);
+            //removing process from ready queue
+            readyQueue.remove(nextReadyProcess);
             allProcessesArrived.clear();
+            swapProcess = false;
+            if(pre_emptive){
+                for(int currentTime = clockTime; currentTime < nextReadyProcess.calculateFinishTime(); currentTime++){
+                    int timeUnitCount = 0;
+                    //loop through ready queue
+                    for(Process process : readyQueue){
+                        //getting all events arrived at that time
+                        //!(find(allProcessesArrived.begin(), allProcessesArrived.end(), process) != allProcessesArrived.end()) &&
+                        if(currentTime >= process.getArrivalTime() && process.getServiceTime() < nextReadyProcess.getServiceTime() - timeUnitCount){
+                            allProcessesArrived.push_back(process);
+                            swapProcess = true;
+                        }
+                    }
+                    if(swapProcess){
+                        allProcessesArrived.sort([](Process &a, Process &b) { return a.getProcessId() < b.getProcessId(); });
+                        break;
+                    }
+                    timeUnitCount++;
+                }
+            }
+            if(!swapProcess){
+                //set clock to finished process time
+                clockTime = nextReadyProcess.calculateFinishTime();
+                //create terminated event
+                currentEvent.setEvent(clockTime, nextReadyProcess, "terminated", "running");
+                //adding event to events (all events that occure in algorithm)
+                events.push_back(currentEvent);
+                if(pre_emptive){
+                    for(int i = 0; i < numOfProcesses; i++){
+                        if(nextReadyProcess.getProcessId() == processes[i].getProcessId()){
+                            //set process start time for processes stored in process array
+                            processes[i].setFinishTime(clockTime);
+                        }
+                    }
+                }
+                clockTime+=switchTime;
+            }
         }
-        //create running event from next ready process
-        currentEvent.setEvent(clockTime, nextReadyProcess, "running", "ready");
-        //adding event to events (all events that occur in algorithm)
-        events.push_back(currentEvent);
-        //set clock to finished process time
-        clockTime = nextReadyProcess.calculateFinishTime() + switchTime;
-        //create terminated event
-        currentEvent.setEvent(clockTime, nextReadyProcess, "terminated", "running");
-        //adding event to events (all events that occure in algorithm)
-        events.push_back(currentEvent);
-        //removing process from ready queue
-        readyQueue.remove(nextReadyProcess);
-        
     }while(!readyQueue.empty()); //while ready queue is not empty
 }
