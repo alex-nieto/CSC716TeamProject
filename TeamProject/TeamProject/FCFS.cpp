b61c22770d76a4582ada3b84664d69a8aaa5eb94
Alex Nieto <alex.nieto7301@gmail.com>
2023-12-15 02:53:34 -0500

    Updates for idle time for fcfs and fix issue with calculateCPUutilization

--- TeamProject/TeamProject/FCFS.cpp
+++ TeamProject/TeamProject/FCFS.cpp
@@ -28,39 +28,57 @@ void FCFS :: implementAlg(){
     Event currentEvent;
     Process nextReadyProcess;
     
+    //the processes that have arrived at a given time
+    list<Process> allProcessesArrived;
+    
     do{
-        //sets next process to execute
-        nextReadyProcess = readyQueue.front();
-        //set next process start time (this is the next/latest start time if process switch happened)
-        nextReadyProcess.setStartTime(clockTime);
-        //save start time info to main process array data member - "master" process list
-        for(int i = 0; i < numOfProcesses; i++){
-            //check if ids match and if we have not already set initial start time (which will be used to calculate other values)
-            if(nextReadyProcess == processes[i] && processes[i].getStartTime() < 0){
-                //set process start time for processes stored in process array
-                processes[i].setStartTime(clockTime);
+        //loop through ready queue
+        for(Process process : readyQueue){
+            //getting all events arrived at that time
+            if(clockTime >= process.getArrivalTime()){
+                allProcessesArrived.push_back(process);
             }
         }
-        //create running event from next ready process
-        currentEvent.setEvent(clockTime, nextReadyProcess, "running", "ready");
-        //adding event to events (all events that occur in algorithm)
-        events.push_back(currentEvent);
-        //removing process from ready queue
-        readyQueue.remove(nextReadyProcess);
-        //set clock to finished process time
-        clockTime = nextReadyProcess.calculateFinishTime();
-        //create terminated event
-        currentEvent.setEvent(clockTime, nextReadyProcess, "terminated", "running");
-        //adding event to events (all events that occure in algorithm)
-        events.push_back(currentEvent);
-        //save the finish time to main process list
-        for(int i = 0; i < numOfProcesses; i++){
-            if(nextReadyProcess == processes[i]){
-                //set process start time for processes stored in process array
-                processes[i].setFinishTime(clockTime);
+        //if no processes available at next time
+        if(allProcessesArrived.empty()){
+            //calculate idle time
+            idleTime = idleTime + readyQueue.front().getArrivalTime() - clockTime;
+            clockTime = readyQueue.front().getArrivalTime();
+        }else{
+            //sets next process to execute
+            nextReadyProcess = readyQueue.front();
+            //set next process start time (this is the next/latest start time if process switch happened)
+            nextReadyProcess.setStartTime(clockTime);
+            //save start time info to main process array data member - "master" process list
+            for(int i = 0; i < numOfProcesses; i++){
+                //check if ids match and if we have not already set initial start time (which will be used to calculate other values)
+                if(nextReadyProcess == processes[i] && processes[i].getStartTime() < 0){
+                    //set process start time for processes stored in process array
+                    processes[i].setStartTime(clockTime);
+                }
             }
+            //create running event from next ready process
+            currentEvent.setEvent(clockTime, nextReadyProcess, "running", "ready");
+            //adding event to events (all events that occur in algorithm)
+            events.push_back(currentEvent);
+            //removing process from ready queue
+            readyQueue.remove(nextReadyProcess);
+            //set clock to finished process time
+            clockTime = nextReadyProcess.calculateFinishTime();
+            //create terminated event
+            currentEvent.setEvent(clockTime, nextReadyProcess, "terminated", "running");
+            //adding event to events (all events that occure in algorithm)
+            events.push_back(currentEvent);
+            //save the finish time to main process list
+            for(int i = 0; i < numOfProcesses; i++){
+                if(nextReadyProcess == processes[i]){
+                    //set process start time for processes stored in process array
+                    processes[i].setFinishTime(clockTime);
+                }
+            }
+            //account for process switch time
+            clockTime+=switchTime;
+            allProcessesArrived.clear();
         }
-        //account for process switch time
-        clockTime+=switchTime;
     }while(!readyQueue.empty()); //while ready queue is not empty
 }
